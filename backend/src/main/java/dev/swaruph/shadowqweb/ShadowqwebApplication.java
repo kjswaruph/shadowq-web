5e65bb740c7485e2ec57c6220f560c6b84393763
swaruph <jyothiswaruphkolli@gmail.com>
2025-04-07 22:25:10 +0530

    base frontend & backend

---
+++ backend/src/main/java/dev/swaruph/shadowqweb/ShadowqwebApplication.java
@@ -0,0 +1,13 @@
+package dev.swaruph.shadowqweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowqwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowqwebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/swaruph/shadowqweb/ShadowqwebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.swaruph.shadowqweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowqwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
